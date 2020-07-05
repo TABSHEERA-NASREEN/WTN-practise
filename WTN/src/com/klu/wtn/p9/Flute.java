765e59a4e36ef58b9190989d6784c0024c769f6c
YOGA <YOGA@DESKTOP-KEIVQ3I>
2020-07-05 20:35:44 +0530

    updated p9

--- WTN/src/com/klu/wtn/p9/Flute.java
+++ WTN/src/com/klu/wtn/p9/Flute.java
@@ -2,6 +2,6 @@ package com.klu.wtn.p9;
 
 public class Flute extends Instrument{
 	public void play() {
-	System.out.println("Flute is playing  toot toot toot toot");
+	System.out.println("Flute is playing toot toot toot toot");
 	}
 }
